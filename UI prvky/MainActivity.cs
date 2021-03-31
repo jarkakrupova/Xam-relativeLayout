467dc9e2c6e32ec51500a11c9bf40dea4b19e798
Jarka <jarmila.krupova@vsb.cz>
2021-03-31 14:24:11 +0200

    switch and event handler added

--- UI prvky/MainActivity.cs
+++ UI prvky/MainActivity.cs
@@ -13,20 +13,39 @@ namespace UI_prvky
         TextView lbPopisek;
         CheckBox cbCheckbox;
         EditText tbText;
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
+        Switch swPrepinac;
+        protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
+            SetupReferences();
+            SubscribeEventHandlers();
+        }
+
+        private void SubscribeEventHandlers() {
+            btnButton.Click += BtnButton_Click;
+            cbCheckbox.Click += CbCheckbox_Click;
+            tbText.Click += TbText_Click;
+            swPrepinac.Click += SwPrepinac_Click;
+        }
+
+        private void SwPrepinac_Click(object sender, System.EventArgs e) {
+            if (swPrepinac.Checked) {
+                lbPopisek.Text = "Switch je v poloze ON";
+            }
+            else {
+                lbPopisek.Text = "Switch je v poloze OFF";
+            }
+        }
+
+        private void SetupReferences() {
             btnButton = FindViewById<Button>(Resource.Id.btnButton);
             lbPopisek = FindViewById<TextView>(Resource.Id.lbPopisek);
             cbCheckbox = FindViewById<CheckBox>(Resource.Id.cbCheckbox);
             tbText = FindViewById<EditText>(Resource.Id.tbText);
-            btnButton.Click += BtnButton_Click;
-            cbCheckbox.Click += CbCheckbox_Click;
-            tbText.Click += TbText_Click;
+            swPrepinac = FindViewById<Switch>(Resource.Id.swPrepinac);
         }
 
         private void TbText_Click(object sender, System.EventArgs e) {
